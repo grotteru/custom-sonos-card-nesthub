08ddafea2609bb5ccc9b38af059410d81d529b55
Magnus Grøtterud <magnus.grotterud@gmail.com>
2023-01-23 22:59:51 +0100

    Ny URL

--- src/cards/custom-sonos-card.ts
+++ src/cards/custom-sonos-card.ts
@@ -1,7 +1,7 @@
 import {AllSections} from './all-sections';
 import {CardConfig} from '../types'; // Deprecated: use AllSections instead
 
-// Deprecated: use AllSections instead
+// Deprecated: use AllSections instead asdf
 export class CustomSonosCard extends AllSections {
   setConfig(config: CardConfig) {
     console.error('type: custom:custom-sonos-card is deprecated, please use custom:sonos-card instead');
